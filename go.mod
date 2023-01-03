dab32af43b2e31abbc56ceedd7eb842feeb7fabc
chinedu imoh <chineduimoh@chinedus-MacBook-Pro.local>
2023-01-04 00:22:15 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/chineduimoh/logrus_airbrake
+
+go 1.19
+
+require github.com/airbrake/gobrake/v5 v5.6.1
+
+require golang.org/x/sys v0.3.0 // indirect
+
+require (
+	github.com/caio/go-tdigest/v4 v4.0.1 // indirect
+	github.com/jonboulle/clockwork v0.3.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.9.0
+)
---
+++ main.go
@@ -0,0 +1,78 @@
+package logrusairbrake
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/airbrake/gobrake/v5"
+
+	"github.com/sirupsen/logrus"
+)
+
+// default levels to be fired when logging on
+var defaultLevels = []logrus.Level{
+	logrus.ErrorLevel,
+	logrus.FatalLevel,
+	logrus.PanicLevel,
+}
+
+// AirbrakeHook to send exceptions to an exception-tracking service compatible
+// with the Airbrake API.
+type AirbrakeHook struct {
+	Airbrake  *gobrake.Notifier
+	UseLevels []logrus.Level
+}
+
+func NewAirbrakeHook(projectID int64, apiKey, env string) *AirbrakeHook {
+	airbrake := gobrake.NewNotifier(projectID, apiKey)
+	airbrake.AddFilter(func(notice *gobrake.Notice) *gobrake.Notice {
+		if env == "development" {
+			return nil
+		}
+		notice.Context["environment"] = env
+		return notice
+	})
+
+	hook := &AirbrakeHook{
+		Airbrake:  airbrake,
+		UseLevels: defaultLevels,
+	}
+	return hook
+}
+
+func (hook *AirbrakeHook) Fire(entry *logrus.Entry) error {
+	var notifyErr error
+	err, ok := entry.Data["error"].(error)
+	if ok {
+		notifyErr = err
+	} else {
+		notifyErr = errors.New(entry.Message)
+	}
+	var req *http.Request
+	for k, v := range entry.Data {
+		if r, ok := v.(*http.Request); ok {
+			req = r
+			delete(entry.Data, k)
+			break
+		}
+	}
+	notice := hook.Airbrake.Notice(notifyErr, req, 3)
+	for k, v := range entry.Data {
+		notice.Context[k] = fmt.Sprintf("%s", v)
+	}
+
+	hook.sendNotice(notice)
+	return nil
+}
+
+func (hook *AirbrakeHook) sendNotice(notice *gobrake.Notice) {
+	if _, err := hook.Airbrake.SendNotice(notice); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to send error to Airbrake: %v\n", err)
+	}
+}
+
+func (hook *AirbrakeHook) Levels() []logrus.Level {
+	return hook.UseLevels
+}
